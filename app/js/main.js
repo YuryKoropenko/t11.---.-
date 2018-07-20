15629bf7fad87ef6f8ec444c045392e7f4053d93
YuryKoropenko <yurykoropenko@gmail.com>
2018-07-20 17:04:20 +0300

    new-commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+
+	/*Меню каталога*/
 	$('.h-catalog__item').hover(function() {
 		$(this).addClass('active');
 		$(this).children('.h-catalog__submenu').stop(false, true).slideDown(200);
@@ -6,4 +8,9 @@ $(function() {
 		$(this).removeClass('active');
 		$(this).children('.h-catalog__submenu').stop(false, true).slideUp(200);
 	});
+	$('.h-catalog__item .h-catalog__link').on('click', function() {
+		return false;
+	});
+
+	
 });
