cd60c229eac70f941126ce667323f093b83f7327
Yury <yurykoropenko@gmail.com>
2018-07-23 18:10:20 +0300

    index

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,40 @@
 $(function() {
 
-	/*Меню каталога*/
-	$('.h-catalog__item').hover(function() {
-		$(this).addClass('active');
-		$(this).children('.h-catalog__submenu').stop(false, true).slideDown(200);
-	}, function() {
-		$(this).removeClass('active');
-		$(this).children('.h-catalog__submenu').stop(false, true).slideUp(200);
-	});
+	/*Онлайн заявка*/
+	$('.h-application').fancybox();
+
 	$('.h-catalog__item .h-catalog__link').on('click', function() {
 		return false;
 	});
 
+	if( $(window).width() >= 992 ) {
+		/*Меню каталога*/
+		$('.h-catalog__item').hover(function() {
+			$(this).addClass('active');
+			$(this).children('.h-catalog__submenu').stop(false, true).slideDown(200);
+		}, function() {
+			$(this).removeClass('active');
+			$(this).children('.h-catalog__submenu').stop(false, true).slideUp(200);
+		});
+	}
+	if( $(window).width() < 992 ) {
+		/*мобильное меню каталога*/
+		$('.h-catalog__mobbtn').on('click', function() {
+			$(this).addClass('active');
+			$('.h-catalog__text').text('Закрыть меню');
+			$('.h-catalog').stop(false, true).slideDown(200);
+			$('.header__h-catalog').css({
+				'background': '#000'
+			});
+			return false;
+		});
+		$('.h-catalog__item').on('click', function() {
+			$(this).toggleClass('active');
+			$(this).children('.h-catalog__submenu').slideToggle(200);
+		});
+	}
+
 	
+
+
 });
