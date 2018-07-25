c1c17271f14546f060cd8de862f12a33cd675114
Yury <yurykoropenko@gmail.com>
2018-07-25 18:03:25 +0300

    index-full

--- app/js/main.js
+++ app/js/main.js
@@ -20,18 +20,50 @@ $(function() {
 	if( $(window).width() < 992 ) {
 		/*мобильное меню каталога*/
 		$('.h-catalog__mobbtn').on('click', function() {
-			$(this).addClass('active');
-			$('.h-catalog__text').text('Закрыть меню');
-			$('.h-catalog').stop(false, true).slideDown(200);
-			$('.header__h-catalog').css({
-				'background': '#000'
-			});
+			$(this).toggleClass('active');
+			$('.header__h-catalog').toggleClass('header__h-catalog-active');
+			$('.h-catalog').stop(false, true).slideToggle(200);
+			$('.h-catalog__text-hide').toggle();
+			$('.h-catalog__text-show').toggle();
 			return false;
 		});
-		$('.h-catalog__item').on('click', function() {
-			$(this).toggleClass('active');
-			$(this).children('.h-catalog__submenu').slideToggle(200);
+		$('.h-catalog__link').on('click', function() {
+			if($(this).hasClass('active') == true) {
+				$(this).removeClass('active');
+				$(this).parent().children('.h-catalog__submenu').slideUp(200);
+			} else {
+				$('.h-catalog__link').removeClass('active');
+				$('.h-catalog__submenu').slideUp(200);
+				$(this).toggleClass('active');
+				$(this).parent().children('.h-catalog__submenu').slideToggle(200);
+			}
 		});
+	}
+
+	/*мобильная навигация*/
+	if( $(window).width() < 768 ) {
+		$('.h-nav__mobbtn').on('click', function() {
+			/*ширина выпадающего меню*/
+			var windowswidth = $(window).width();
+			var widthcont = $('.header__top .row').width();
+			var widthleft = -((windowswidth - widthcont) / 2);
+			$('.h-nav').width(windowswidth);
+			$('.h-nav').css({
+				'margin-left': widthleft
+			});
+			/*Выпадающее меню*/
+			$(this).toggleClass('active');
+			$('.header__top').toggleClass('header__top-active');
+			$('.h-nav').stop(false, true).slideToggle(200);
+			$('.h-nav__text-hide').toggle();
+			$('.h-nav__text-show').toggle();
+			return false;
+		});
+	}
+
+	/*замена картинки на телефоне footer*/
+	if( $(window).width() < 768 ) {
+		$('.footer').css('background-image', 'url(img/footer-mob-bg.jpg)');
 	}
 
 	
